2188640d612aba3207d66fe863b33d29888be7e6
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-06-12 09:09:42 +0300

    word and sentence counter

---
+++ wordCounter/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+import string
+import re
+
+def home(request):
+    return render(request, 'home.html')
+
+def count2(request):
+    return render(request, 'count2.html')
+
+def count(request):
+    fullText = request.GET['fullText']
+    lowerFullText = fullText.lower()
+    newFullText = ""
+
+    for i in lowerFullText:
+        if i not in string.punctuation:
+            newFullText += i
+
+    wordlist = newFullText.split()
+    sentenceslist = re.split(r'[.!?]+', fullText)
+
+    wordDictionary = {}
+
+    for word in wordlist:
+        if word in wordDictionary:
+            #Increase
+            wordDictionary[word] += 1
+        else:
+            #Add to the dicitonary
+            wordDictionary[word] = 1
+    sort_wordDictionary = sorted(wordDictionary.items(), key=lambda x: (x[1], x[0]), reverse=True)
+    return render(request, 'count.html', {'fullText':fullText, 'count':len(wordlist), 'wordDictionary': sort_wordDictionary, 'sentencesCount':len(sentenceslist)-1})
